a834c917cc9733fb190f3e88c0c6ac5fb170509a
imedvedec <ivan.medvedec0@gmail.com>
2021-08-29 19:08:58 +0200

    Caesar cipher basic decrypt

--- src/caesar.rs
+++ src/caesar.rs
@@ -1,12 +1,23 @@
 const SHIFT: usize = 1;
 
-pub fn encrypt(input: &[u8], alphabet: &[u8]) -> Vec<u8> {
-    let mut output = vec![0_u8; input.len()];
+pub fn encrypt(plain_text: &[u8], alphabet: &[u8]) -> Vec<u8> {
+    let mut cipher_text = vec![0_u8; plain_text.len()];
 
-    input
+    plain_text
         .iter()
         .enumerate()
-        .for_each(|(i, _)| output[i] = alphabet[(i + SHIFT) % alphabet.len()]);
+        .for_each(|(i, _)| cipher_text[i] = alphabet[(i + SHIFT) % alphabet.len()]);
 
-    return output;
+    cipher_text
+}
+
+pub fn decrypt(cipher_text: &[u8], alphabet: &[u8]) -> Vec<u8> {
+    let mut plain_text = vec![0_u8; cipher_text.len()];
+
+    cipher_text
+        .iter()
+        .enumerate()
+        .for_each(|(i, _)| plain_text[i] = alphabet[(alphabet.len() + i - SHIFT) % alphabet.len()]);
+
+    plain_text
 }
