70ceaca9334e05a1f052d2f626919d69fae5a5e2
imedvedec <ivan.medvedec0@gmail.com>
2021-08-29 20:01:07 +0200

    Caesar cipher enc/dec logic fix

--- src/caesar.rs
+++ src/caesar.rs
@@ -13,15 +13,27 @@ impl<'a> Config<'a> {
             alphabet: DEFAULT_ALPHABET.as_bytes(),
         }
     }
+
+    fn get_symbol_position(&self, symbol: u8) -> usize {
+        let mut symbol_alphabet_position = 0_usize;
+
+        self.alphabet.iter().enumerate().for_each(|(i, x)| {
+            if *x == symbol {
+                symbol_alphabet_position = i
+            }
+        });
+
+        symbol_alphabet_position
+    }
 }
 
 pub fn encrypt(plain_text: &[u8], cfg: &Config) -> Vec<u8> {
     let mut cipher_text = vec![0_u8; plain_text.len()];
 
-    plain_text
-        .iter()
-        .enumerate()
-        .for_each(|(i, _)| cipher_text[i] = cfg.alphabet[(i + cfg.shift) % cfg.alphabet.len()]);
+    plain_text.iter().enumerate().for_each(|(i, symbol)| {
+        cipher_text[i] =
+            cfg.alphabet[(cfg.get_symbol_position(*symbol) + cfg.shift) % cfg.alphabet.len()]
+    });
 
     cipher_text
 }
@@ -29,8 +41,10 @@ pub fn encrypt(plain_text: &[u8], cfg: &Config) -> Vec<u8> {
 pub fn decrypt(cipher_text: &[u8], cfg: &Config) -> Vec<u8> {
     let mut plain_text = vec![0_u8; cipher_text.len()];
 
-    cipher_text.iter().enumerate().for_each(|(i, _)| {
-        plain_text[i] = cfg.alphabet[(cfg.alphabet.len() + i - cfg.shift) % cfg.alphabet.len()]
+    cipher_text.iter().enumerate().for_each(|(i, symbol)| {
+        plain_text[i] = cfg.alphabet[(cfg.alphabet.len() + cfg.get_symbol_position(*symbol)
+            - cfg.shift)
+            % cfg.alphabet.len()]
     });
 
     plain_text
